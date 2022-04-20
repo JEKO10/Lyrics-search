c57aa7b743410b39255ed496b75e92ee64e78394
Aleksa Bubanja <aleksa3b@gmail.com>
2022-04-20 23:28:25 +0200

    displaying top songs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import TopSongs from "./components/TopSongs";
 
 function App() {
   const [songs, setSongs] = useState([]);
@@ -6,18 +7,21 @@ function App() {
 
   const fetchSongs = async () => {
     const response = await fetch(
-      `https://api.musixmatch.com/ws/1.1/chart.artists.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${key}`
+      `https://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${key}`
     );
     const data = await response.json();
-    setSongs(data);
-    console.log(data.message.body.artist_list);
+    setSongs(data.message.body.track_list);
   };
 
   useEffect(() => {
     fetchSongs();
   }, []);
 
-  return <></>;
+  return (
+    <main>
+      <TopSongs songs={songs} />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/TopSongs.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+function TopSongs({ songs }) {
+  return (
+    <section className="topSongs">
+      {songs.map((song) => {
+        console.log(song.track);
+        return (
+          <div key={song.track.track_id}>
+            <h3>{song.track.artist_name}</h3>
+            <h4>
+              <span>Track:</span> {song.track.track_name}
+            </h4>
+            <h4>
+              <span>Album:</span> {song.track.album_name}
+            </h4>
+            <Link to={`/track/${song.track.track_id}`}>View Lyrics</Link>
+          </div>
+        );
+      })}
+    </section>
+  );
+}
+
+export default TopSongs;
